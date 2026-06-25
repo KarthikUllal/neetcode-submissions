e4533bb95c3b532d2aeba50003fbf48ab5705a92
KarthikUllal <karthik2003ku@gmail.com>
2026-06-25 22:41:44 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        if(this.stack.length === 0) return false
+
+        this.stack.pop()
+
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        let min = Infinity
+
+        for(let i = 0; i < this.stack.length; i++){
+            if(this.stack[i] < min){
+                min = this.stack[i]
+            }
+        }
+        return min
+    }
+}
