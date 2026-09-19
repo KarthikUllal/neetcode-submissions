50b753227e577b7337a3ec099fc7c2e1aba9dc4f
KarthikUllal <karthik2003ku@gmail.com>
2026-09-19 21:38:48 +0530

    Add: assign-cookies - submission-4

---
+++ Data Structures & Algorithms/assign-cookies/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i = 0
+
+        for cookie in s:
+            if i < len(g) and cookie >= g[i]:
+                i += 1
+
+        return i
